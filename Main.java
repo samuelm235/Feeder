470d7671a80d2393f3434574eba6b7b9e3516b38
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2024-09-25 13:23:30 +0000

    update

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
-public class Main{
-    public static void main(String[] args){
-int random_birds = (int)(Math.random() * 40 + 10);
-Feeder i = new Feeder(10000);
-System.out.println("number of days birds/bears found food: " + i.simulateManyDays(random_birds, 100));
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Feeder a = new Feeder(2400);
+        System.out.println("number of days birds/bears found food: " + a.simulateManyDays(10, 4));
+        Feeder b = new Feeder(250);
+        System.out.println("number of days birds/bears found food: " + b.simulateManyDays(10, 5));
+        Feeder c = new Feeder(0);
+        System.out.println("number of days birds/bears found food: " + c.simulateManyDays(5, 10));
+        Feeder d = new Feeder(500);
+        d.simulateOneDay(12);
+        System.out.println("Food left: " + d.getCurrentFood());
+        Feeder e = new Feeder(1000);
+        e.simulateOneDay(22);
+        System.out.println("Food left: " + e.getCurrentFood());
+        Feeder f = new Feeder(100);
+        f.simulateOneDay(5);
+        System.out.println("Food left: " + f.getCurrentFood());
 
-System.out.println("Amount of birds: " + random_birds);
-}
+    }
 }
